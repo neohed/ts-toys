75da1cfcc4c8ac246eb563f28b1f0cef41e79fa0
neohed <35173474+neohed@users.noreply.github.com>
2023-04-05 21:05:33 +0100

    Update union-intersection.ts

--- src/union-intersection.ts
+++ src/union-intersection.ts
@@ -5,15 +5,30 @@ type M1 = keyof (A | B);
 type N1 = keyof (A & B);
 
 type X = {
-  id: number;
-  name: string;
+    id: number;
+    name: string;
 }
 type Y = {
-  id: number;
-  age: number;
+    id: number;
+    age: number;
 }
 
 type M2 = keyof (X | Y);
 type M2_ = (keyof X) & (keyof Y);
 type N2 = keyof (X & Y);
 type N2_ = (keyof X) | (keyof Y);
+
+interface Person {
+    name: string;
+}
+
+interface Lifespan {
+    birth: Date;
+}
+
+type P = keyof Person;
+type L = keyof Lifespan;
+type PL_union = string | number; // Something can be either string OR number, but...
+type PL_intersect = string & number; // nothing can be both a string AND a number.
+type PL = keyof (Person & Lifespan);
+type PL_ = keyof (Person | Lifespan);
